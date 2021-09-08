7e12e9ee0dfc4007307a2b1ec2c7802d729c816e
Jérémy Lal <kapouer@melix.org>
2021-09-08 11:48:29 +0200

    Fix for latest postinstall

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const exec = require('util').promisify(require('child_process').exec);
+const Path = require('path');
 
 module.exports = function(inputs, output, options) {
 	return Promise.all(inputs.map((input) => {
 		return exec(`patch --backup --forward --strip 0 --quiet --reject-file - <${output}`, {
-			cwd: input
+			cwd: Path.dirname(input)
 		}).catch(function(err) {
 			if (err.stdout && (err.stdout.indexOf('File to patch') > 0 || !err.stdout.indexOf('ignored'))) {
 				// eslint-disable-next-line no-console
